a0b4f31d4c40b152c81fc1525697a69943cdb51c
Love Gupta <71257080+loveguptapp@users.noreply.github.com>
2021-03-24 02:41:51 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "127.0.0.1" ;
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
